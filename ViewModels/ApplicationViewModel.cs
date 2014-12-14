59efc7c30a7cb8a51d51cbf540168fc10fdeb705
Sabrina <sabrina_privat@hotmail.com>
2014-12-14 21:29:03 +0100

    Still working on the initial architecture. Added a sqlite database.

---
+++ ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundTheClock.ViewModels
+{
+    // Responsibilities:
+    //   - overall state of application
+    //   - navigation 
+    //   - error messages
+    class ApplicationViewModel
+    {
+    }
+}
