e15ee36c774bdf8697171ba297b64bfd20dbc4e5
SabrinaMH <sabrina_privat@hotmail.com>
2015-01-25 11:03:14 +0100

    Refactored mockserver due to depreciated call

--- tests/mocks/mockserver.js
+++ tests/mocks/mockserver.js
@@ -4,7 +4,7 @@ var app = express();
 var customers = {"Customer":[{"Name":"EnergiMidt","Projects":{"Project":{"Name":"Mit EnergiMidt v3","Tasks":{"Task":{"Name":"Udvikling"}}}}},{"Name":"Mjølner"}]};
  
 app.get('/customer', function(req, res) {
-    res.json(200, customers);
+	res.status(200).json(customers);
 });
  
 app.listen(process.env.PORT || 9001);
